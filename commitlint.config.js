d44929e82bc2a6d6c934ea6d848aa1bd2797b2e8
y-jaga <yogeshjaga123@gmail.com>
2026-03-29 11:55:44 +0530

    feat: eslint setup

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,28 +1,3 @@
-// module.exports = {
-//   extends: ["@commitlint/cli", "@commitlint/config-conventional"],
-//   commitlint: "Unknown word",
-//   rules: {
-//     "type-enum": [
-//       2,
-//       "always",
-//       [
-//         "feat",
-//         "fix",
-//         "docs",
-//         "style",
-//         "refactor",
-//         "perf",
-//         "test",
-//         "build",
-//         "ci",
-//         "chore",
-//         "revert",
-//       ],
-//     ],
-//     "subject-case": [2, "always", "sentence-case"],
-//   },
-// };
-
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  eslint.configs.recommended,
+
+  ...tseslint.configs.recommendedTypeChecked,
+
+  {
+    files: ["**/*.ts"],
+
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+
+    rules: {
+      // ❗ Your custom rules
+      "no-console": "warn", // better than error in backend
+      quotes: ["error", "single", { allowTemplateLiterals: true }],
+
+      // 🔥 Important TS rules
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-floating-promises": "error",
+    },
+  },
+];
