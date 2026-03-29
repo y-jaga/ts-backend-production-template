bbbdc2c6b1111ac250d95baeebf9903788ef9b2b
y-jaga <yogeshjaga123@gmail.com>
2026-03-29 13:01:48 +0530

    feat: prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,34 @@
 // @ts-check
 
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
 export default [
-  eslint.configs.recommended,
+    eslint.configs.recommended,
 
-  ...tseslint.configs.recommendedTypeChecked,
+    ...tseslint.configs.recommendedTypeChecked,
 
-  {
-    files: ["**/*.ts"],
+    {
+        files: ['**/*.ts'],
 
-    languageOptions: {
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+        languageOptions: {
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: import.meta.dirname
+            }
+        },
+
+        rules: {
+            // ❗ Your custom rules
+            'no-console': 'warn', // better than error in backend
+            quotes: ['error', 'single', { allowTemplateLiterals: true }],
+
+            // 🔥 Important TS rules
+            '@typescript-eslint/no-unused-vars': 'error',
+            '@typescript-eslint/no-floating-promises': 'error'
+        }
     },
-
-    rules: {
-      // ❗ Your custom rules
-      "no-console": "warn", // better than error in backend
-      quotes: ["error", "single", { allowTemplateLiterals: true }],
-
-      // 🔥 Important TS rules
-      "@typescript-eslint/no-unused-vars": "error",
-      "@typescript-eslint/no-floating-promises": "error",
-    },
-  },
-];
+    // MUST BE LAST
+    eslintConfigPrettier
+]
