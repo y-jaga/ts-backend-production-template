81e47c102bd843568ac030d6ebc46e77d1ec2b85
y-jaga <yogeshjaga123@gmail.com>
2026-03-31 07:54:19 +0530

    feat: environment setup

---
+++ src/config/config.ts
@@ -0,0 +1,12 @@
+import dotenvFlow from 'dotenv-flow'
+
+dotenvFlow.config()
+
+console.log(process.env)
+
+export default {
+    ENV: process.env.ENV,
+    PORT: process.env.PORT,
+    SERVER_URL: process.env.SERVER_URL,
+    DATABASE_URL: process.env.DATABASE_URL
+}
